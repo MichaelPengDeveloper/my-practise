3124a600bec227e1aed44720136e704791c5ddd7
wangpeng <www.277214756@qq.com>
2017-07-05 16:13:26 +0800

    'fix'

--- src/test/java/com/test/Test.java
+++ src/test/java/com/test/Test.java
@@ -1,5 +1,6 @@
 package com.test;
 
+import com.test.singleton.Singleton;
 import com.wp.practise.model.User;
 import com.wp.practise.service.OrderService;
 import com.wp.practise.service.UserService;
@@ -11,17 +12,60 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by Administrator on 2017/6/4.
  */
 public class Test{
 
-    public static void main(String[] args){
+    boolean lock;
 
-        System.out.println(Character.isJavaIdentifierStart('a'));
+    public boolean isLock() {
+        return lock;
+    }
+
+    public void setLock(boolean lock) {
+        this.lock = lock;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final Set<String> strings = Collections.synchronizedSet(new HashSet<String>());
+
+        final Test test = new Test();
+
+        test.setLock(true);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(new Runnable() {
+
+                public void run() {
+                    while (true) {
+                        System.out.println(Thread.currentThread().getName() + "" +!test.isLock());
+                        if (!test.isLock()) {
+                            Singleton singleton = Singleton.getInstanceLazy();
+                            strings.add(singleton.toString());
+                            break;
+                        }
+                    }
+                }
+            });
+
+        }
+
+        Thread.sleep(500);
+        test.setLock(false);
+        Thread.sleep(500);
+        System.out.println("------并发情况下我们取到的实例------");
+        for (String instance : strings) {
+            System.out.println(instance);
+        }
+        executorService.shutdown();
 
     }
 
